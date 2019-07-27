a5d67e4175de712a010d381c284b66a97db0ddd5
RJ Rosa <ronaldjavierrosa2274@gmail.com>
2019-07-27 04:51:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ require 'pry'
 def reverse_each_word(user_input)
   reversed_words = []
 
-  user_input.split(" ").each do |word|
+  user_input.split(" ").collect do |word|
     reversed_words << word.reverse
   end
  reversed_words.join(" ")
